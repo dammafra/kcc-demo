0f2888d2b5d52fbf4041c95362588ae24dc62b22
Francesco Dammacco <francesco.dammacco@apuliasoft.com>
2018-05-24 00:16:55 +0200

    generic warning for entry in hosts file in init and cleanup scripts

--- kubernetes/finals/cleanup.sh
+++ kubernetes/finals/cleanup.sh
@@ -4,6 +4,4 @@ kubectl delete secret credentials-secret
 kubectl delete ingress kcc-ingress
 kubectl delete deployment backend frontend
 kubectl delete service backend-service frontend-service
-
-sed -i '$ d' /cygdrive/c/Windows/System32/drivers/etc/hosts
-echo "removed entry 'kcc.local' from hosts file"
+echo "you have to manually remove entry '$(minikube ip) kcc.local' from hosts file"
--- kubernetes/finals/init.sh
+++ kubernetes/finals/init.sh
@@ -4,6 +4,4 @@ kubectl apply -f secrets/
 kubectl apply -f deployments/
 kubectl apply -f services/
 kubectl apply -f ingresses/
-
-echo "$(minikube ip) kcc.local" >> /cygdrive/c/Windows/System32/drivers/etc/hosts
-echo "added entry 'kcc.local' in hosts file"
+echo "you have to manually add entry '$(minikube ip) kcc.local' in hosts file"
